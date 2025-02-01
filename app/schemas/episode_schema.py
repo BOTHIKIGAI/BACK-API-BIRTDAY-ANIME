b2aec6f70ca3840cea3f5a40ab9204a6ae1ce907
BOTHIKIGAI <estebax89@gmail.com>
2025-02-01 15:50:50 -0500

    feat: add sanitization methods for arc and name fields in EpisodeSchema

--- app/schemas/episode_schema.py
+++ app/schemas/episode_schema.py
@@ -3,7 +3,8 @@ This module contains the schema to define and
 validate the Episode data structure.
 """
 from datetime import date
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
+from typing import Optional
 
 
 class EpisodeSchema(BaseModel):
@@ -20,11 +21,62 @@ class EpisodeSchema(BaseModel):
 
     # Attributes
     anime_id: int
-    arc: str
+    arc: Optional[str] = None
     temp: int
     name: str
     episode: int
     air_date: date
+
+
+    # Sanitize Functions
+    @field_validator('arc', 'name', mode = 'before')
+    def sanitize_string(cls, v):
+        """
+        Sanitizes string fields by stripping whitespace
+        and converting to lowercase.
+
+        Args:
+            v (str): The string value to sanitize.
+
+        Returns:
+            str: The sanitized string.
+        """
+        if v is None:
+            return v
+
+        v = cls.remove_extra_spaces(v)
+        v = cls.convert_to_lowercase(v)
+
+        return v
+
+    # Functions to sanitize data
+    @staticmethod
+    def remove_extra_spaces(value: str) -> str:
+        """
+        Removes extra spaces between words.
+
+        Args:
+            value (str): The string value to process.
+
+        Returns:
+            str: The string with extra spaces removed.
+        """
+        return ' '.join(value.split())
+
+
+    @staticmethod
+    def convert_to_lowercase(value: str) -> str:
+        """
+        Converts the string to lowercase.
+
+        Args:
+            value (str): The string value to process.
+
+        Returns:
+            str: The string converted to lowercase.
+        """
+        return value.lower()
+
 
     class Config:
         """
