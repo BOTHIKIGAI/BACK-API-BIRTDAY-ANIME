824c7f76352a69bfd53abaa5d7f0352b785c31c8
BOTHIKIGAI <estebax89@gmail.com>
2025-02-01 14:40:32 -0500

    feat: add sanitization methods for anime name and category fields

--- app/schemas/anime_schema.py
+++ app/schemas/anime_schema.py
@@ -3,7 +3,7 @@ This module contains the schema to define and
 validate the Anime data structure.
 """
 from datetime import date
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 
 
 class AnimeSchema(BaseModel):
@@ -21,6 +21,54 @@ class AnimeSchema(BaseModel):
     name: str
     category: str
     release_date: date
+
+
+    # Sanitize Functions
+    @field_validator('name', 'category', mode = 'before')
+    def sanitize_string(cls, v):
+        """
+        Sanitizes string fields by stripping whitespace and converting to lowercase.
+
+        Args:
+            v (str): The string value to sanitize.
+
+        Returns:
+            str: The sanitized string.
+        """
+        v = cls.remove_extra_spaces(v)
+        v = cls.convert_to_lowercase(v)
+
+        return v
+
+
+    # Functions to sanitize data
+    @staticmethod
+    def remove_extra_spaces(value: str) -> str:
+        """
+        Removes extra spaces between words.
+
+        Args:
+            value (str): The string value to process.
+
+        Returns:
+            str: The string with extra spaces removed.
+        """
+        return ' '.join(value.split())
+
+
+    @staticmethod
+    def convert_to_lowercase(value: str) -> str:
+        """
+        Converts the string to lowercase.
+
+        Args:
+            value (str): The string value to process.
+
+        Returns:
+            str: The string converted to lowercase.
+        """
+        return value.lower()
+
 
     class Config:
         """
