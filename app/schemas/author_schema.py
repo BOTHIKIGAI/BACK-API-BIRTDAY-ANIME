fdc62ef1a01664fccd8e7c92558089f752f6b3d9
BOTHIKIGAI <estebax89@gmail.com>
2025-02-01 14:13:15 -0500

    feat: add sanitization methods for author name and alias fields

--- app/schemas/author_schema.py
+++ app/schemas/author_schema.py
@@ -3,7 +3,7 @@ This module contains the schema to define and
 validate the Author data structure.
 """
 from datetime import date
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 from typing import Optional
 
 
@@ -22,6 +22,57 @@ class AuthorSchema(BaseModel):
     name: Optional[str] = None
     alias: Optional[str] = None
     birthday: Optional[date] = None
+
+
+    # Sanitize Functions
+    @field_validator('name', 'alias', mode = 'before')
+    def sanitize_string(cls, v):
+        """
+        Sanitizes string fields by stripping whitespace and converting to lowercase.
+
+        Args:
+            v (str): The string value to sanitize.
+
+        Returns:
+            str: The sanitized string.
+        """
+        if v is None:
+            return v
+
+        v = cls.remove_extra_spaces(v)
+        v = cls.convert_to_lowercase(v)
+
+        return v
+
+
+    # Functions to sanitize data
+    @staticmethod
+    def remove_extra_spaces(value: str) -> str:
+        """
+        Removes extra spaces between words.
+
+        Args:
+            value (str): The string value to process.
+
+        Returns:
+            str: The string with extra spaces removed.
+        """
+        return ' '.join(value.split())
+
+
+    @staticmethod
+    def convert_to_lowercase(value: str) -> str:
+        """
+        Converts the string to lowercase.
+
+        Args:
+            value (str): The string value to process.
+
+        Returns:
+            str: The string converted to lowercase.
+        """
+        return value.lower()
+
 
     class Config:
         """
