6e00c0d7987a1491ad10b99e804403dbc1cdb620
BOTHIKIGAI <estebax89@gmail.com>
2025-02-08 13:45:42 -0500

    refactor: standardize environment variable loading syntax

--- app/config/database.py
+++ app/config/database.py
@@ -11,17 +11,18 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 # load env variable
 load_dotenv()
 
-DATABASE_MOTOR = os.getenv('database_motor')
-USERNAME_DB = os.getenv('username_db')
-PASSWORD_DB = os.getenv('password_db')
-HOST_PORT = os.getenv('host_port')
-NAME_DB = os.getenv('name_database')
+DATABASE_MOTOR = os.getenv("database_motor")
+USERNAME_DB = os.getenv("username_db")
+PASSWORD_DB = os.getenv("password_db")
+HOST_PORT = os.getenv("host_port")
+NAME_DB = os.getenv("name_database")
 
 DATABASE_URL = f"{DATABASE_MOTOR}://{USERNAME_DB}:{PASSWORD_DB}@{HOST_PORT}/{NAME_DB}"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
 
 def get_db_connection():
     """
