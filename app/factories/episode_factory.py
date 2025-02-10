f3395288e5e223aaa4ecbb4fc03a7b0925401e69
BOTHIKIGAI <estebax89@gmail.com>
2025-02-10 14:43:54 -0500

    correccion: arreglar indentacion, organizacion de imporaciones

--- app/factories/episode_factory.py
+++ app/factories/episode_factory.py
@@ -1,8 +1,10 @@
 """
 This module the factory for episode.
 """
+
 from app.models.tables.episode_models import Episode
 from app.schemas.episode_schema import EpisodeSchema
+
 
 class EpisodeFactory:
     """
@@ -15,11 +17,14 @@ class EpisodeFactory:
     Returns:
         Episode: The created Episode instance.
     """
+
     @staticmethod
     def create(episode_body: EpisodeSchema) -> Episode:
-        return Episode(anime_id = episode_body.anime_id,
-                       arc = episode_body.arc,
-                       season = episode_body.season,
-                       name = episode_body.name,
-                       episode = episode_body.episode,
-                       air_date = episode_body.air_date)
+        return Episode(
+            anime_id=episode_body.anime_id,
+            arc=episode_body.arc,
+            season=episode_body.season,
+            name=episode_body.name,
+            episode=episode_body.episode,
+            air_date=episode_body.air_date,
+        )
