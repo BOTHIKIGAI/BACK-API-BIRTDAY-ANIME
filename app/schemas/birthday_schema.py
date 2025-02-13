fd4239f52dd3671fd2bbc5f5c339b34ea7b0c85f
BOTHIKIGAI <estebax89@gmail.com>
2025-02-13 17:12:32 -0500

    birthday_schema: add birthday schema for validating date coincidence with anime, episodes, and authors

---
+++ app/schemas/birthday_schema.py
@@ -0,0 +1,27 @@
+"""
+This module contains the schema to define and validate the consult
+the coincidence of a date with anime, episodes and authors.
+"""
+
+from typing import List
+
+from pydantic import BaseModel
+
+from app.schemas.anime_schema import AnimeSchema
+from app.schemas.author_schema import AuthorSchema
+
+
+class BirthdaySchemaResponse(BaseModel):
+    """
+    Schema for the response of a birthday match query.
+
+    Attributes:
+        anime (List[AnimeSchema]): List of anime that match the given date.
+        author (List[AuthorSchema]): List of authors that match the given date.
+        episode (int): Number of episodes that match the given date.
+    """
+
+    # Attributes
+    anime: List[AnimeSchema]
+    author: List[AuthorSchema]
+    episode: int = 0
