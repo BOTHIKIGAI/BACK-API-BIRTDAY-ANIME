2cb12893f06ce7d05960a67e9726c244a19ef762
BOTHIKIGAI <estebax89@gmail.com>
2025-02-12 20:13:01 -0500

    author_factory: remove class AuthorAnime

--- app/factories/author_factory.py
+++ app/factories/author_factory.py
@@ -3,7 +3,7 @@ This module the factory for author
 """
 
 from app.models.tables.author_models import Author
-from app.schemas.author_schema import AuthorAnimeRelationSchema, AuthorSchema
+from app.schemas.author_schema import AuthorSchema
 
 
 class AuthorFactory:
@@ -33,23 +33,3 @@ class AuthorFactory:
             alias=author_body.alias,
             birthday=author_body.birthday,
         )
-
-
-class AuthorAnimeFactory:
-    """
-    A factory class for creating a dictionary representation of an author-anime relationship.
-
-    This class provides a static method to convert an instance of
-    AuthorAnimeRelationSchema into a dictionary containing the author and anime identifiers.
-
-    Methods:
-        create(data_relation: AuthorAnimeRelationSchema) -> dict:
-            Converts the given data_relation into a dictionary with keys 'author_id'
-            and 'anime_id'.
-    """
-    @staticmethod
-    def create(data_relation: AuthorAnimeRelationSchema) -> dict:
-        return {
-            "author_id": data_relation.author_id,
-            "anime_id": data_relation.anime_id,
-        }
