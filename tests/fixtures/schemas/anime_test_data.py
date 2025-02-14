bc962024af841e2534a416d35dc0d51b1c091a58
BOTHIKIGAI <estebax89@gmail.com>
2025-02-14 14:05:55 -0500

    anime_test_data: creation of cases for the evaluation of the attribute release date for the anime scheme tests

---
+++ tests/fixtures/schemas/anime_test_data.py
@@ -0,0 +1,18 @@
+"""
+This module contains the data structures with the test information.
+"""
+
+RELEASE_DATES_NOT_VALID = {
+    "future_date": {
+        "release_date": "2999-02-14",
+        "error_message": "The date should not be in the future."
+    },
+    "too_early_date": {
+        "release_date": "1906-12-31",
+        "error_message": "Date 1906-12-31 is too early. Minimum allowed date is 1907-01-01"
+    },
+    "invalid_format": {
+        "release_date": "not-a-date",
+        "error_message": "Invalid date format: 'not-a-date'. Date must be in ISO format (YYYY-MM-DD)"
+    }
+}
