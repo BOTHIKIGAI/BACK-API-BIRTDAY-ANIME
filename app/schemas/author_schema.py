76689a4f4817c27135107a794d3e9e40c87eced4
BOTHIKIGAI <estebax89@gmail.com>
2025-02-17 10:39:06 -0500

    author_schema: refactoring of validations and sanitization of information

--- app/schemas/author_schema.py
+++ app/schemas/author_schema.py
@@ -1,89 +1,95 @@
 """
-This module contains the schema to define and
-validate the Author data structure.
+This module contains the schema to define and validate the Author data structure.
 """
 from datetime import date
 
 from pydantic import BaseModel, field_validator
 
+from app.utils.sanatizers.common_sanatizers_str import (
+    convert_to_lowercase,
+    remove_extra_spaces,
+)
+from app.utils.validations.common_validations_date import (
+    create_date,
+    validate_date_format,
+    validate_date_no_less_minimum,
+    validate_date_not_in_the_future,
+)
+from app.utils.validations.common_validations_str import validate_is_instance_str
 
-class AuthorSchema(BaseModel):
-    """
-    Represents the schema for the validation of
-    the Author object data.
 
-    Attributes:
-        name (str): The name of the author.
-        alias (str): The alias of the author.
-        birthday (date): The birthday of the author.
-    """
+class AuthorSchemaResponse(BaseModel):
+    name: str
+    alias: str
+    birthday: date
 
-    # Attributes
-    name: str = ""
+
+class AuthorSchemaCreate(BaseModel):
+    name: str
     alias: str = ""
-    birthday: date = date.fromisoformat("1900-01-01")
+    birthday: date
 
 
-    # Sanitize Functions
-    @field_validator('name', 'alias', mode = 'before')
-    def sanitize_string(cls, v):
+    # Validate data
+    @field_validator('name', 'alias', mode="before")
+    def validate_str(cls, attribute: str) -> str:
         """
-        Sanitizes string fields by stripping whitespace
-        and converting to lowercase.
+        Validate if the attribute is a string.
 
         Args:
-            v (str): The string value to sanitize.
+            attribute (str): The string to validate.
+
+        Returns:
+            str: The validated string.
+
+        Raises:
+            ValueError: If the attribute is not a string.
+        """
+        validate_is_instance_str(attribute)
+        return attribute
+
+
+    @field_validator('birthday', mode="before")
+    def validate_date(cls, attribute: str) -> date:
+        """
+        Validate the release date format and constraints.
+
+        Args:
+            attribute (str): The date string to validate.
+
+        Returns:
+            date: The validated date object.
+
+        Raises:
+            ValueError: If the date format is invalid or constraints are not met.
+        """
+        validate_is_instance_str(attribute)
+        validate_date_format(attribute)
+        attribute_date = create_date(attribute)
+        validate_date_not_in_the_future(attribute_date)
+        min_date = date(1877, 11, 4)
+        validate_date_no_less_minimum(
+            value=attribute_date,
+            min_date=min_date
+        )
+        return attribute_date
+
+
+    # Data sanitization
+    @field_validator('name', 'alias', mode="after")
+    def sanatizar_str(cls, attribute: str) -> str:
+        """
+        Sanitize string attributes by removing extra spaces and converting to lowercase.
+
+        Args:
+            attribute (str): The string to sanitize.
 
         Returns:
             str: The sanitized string.
         """
-        if v is None:
-            return v
-
-        v = cls.remove_extra_spaces(v)
-        v = cls.convert_to_lowercase(v)
-
-        return v
-
-
-    # Functions to sanitize data
-    @staticmethod
-    def remove_extra_spaces(value: str) -> str:
-        """
-        Removes extra spaces between words.
-
-        Args:
-            value (str): The string value to process.
-
-        Returns:
-            str: The string with extra spaces removed.
-        """
-        return ' '.join(value.split())
-
-
-    @staticmethod
-    def convert_to_lowercase(value: str) -> str:
-        """
-        Converts the string to lowercase.
-
-        Args:
-            value (str): The string value to process.
-
-        Returns:
-            str: The string converted to lowercase.
-        """
-        return value.lower()
-
-
-    class Config:
-        """
-        Configuration class for Pydantic model.
-
-        Attributes:
-            from_attributes (bool): Indicates if the model
-            should be populated from attributes.
-        """
-        from_attributes = True
+        attribute = remove_extra_spaces(attribute)
+        attribute = convert_to_lowercase(attribute)
+        return attribute
 
 
 class AuthorAnimeRelationSchema(BaseModel):
