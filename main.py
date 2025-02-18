63999cd8bcc5587425f28eb494497f9a97903768
BOTHIKIGAI <estebax89@gmail.com>
2025-02-18 14:56:28 -0500

    main and .gitignore: middleware implementation to retrieve logs and update gitignore

--- main.py
+++ main.py
@@ -5,16 +5,26 @@ This module initializes the FastAPI application and includes the router for hand
 requests related to anime episode release dates.
 """
 
+import logging
+
 from fastapi import FastAPI
 
+from app.config.logging import setup_logging
+from app.middleware.logging_middleware import log_requests
 from app.routers.v1.anime_router import AnimeRouter
 from app.routers.v1.author_router import AuthorRouter
 from app.routers.v1.birthday_router import BirthdayRouter
 from app.routers.v1.episode_router import EpisodeRouter
 
+setup_logging()
+logger = logging.getLogger("BIRTHDAY_ANIME")
+
 app = FastAPI(
     title="BIRTHDAY ANIME",
-    description="API for the consumption of episode release dates.")
+    description="API for the consumption of episode release dates."
+)
+
+app.middleware("http")(log_requests)
 
 app.include_router(AuthorRouter, tags=["Author"], prefix='/v1/author')
 app.include_router(AnimeRouter, tags=["Anime"], prefix='/v1/anime')
